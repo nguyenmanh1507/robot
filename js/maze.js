2ff8d5c3ad69cf0069dc1f6cfb76c7757ef9dd4b
nguyen manh <nguyenmanh1507@gmail.com>
2015-11-01 14:13:36 +0700

    update

--- js/maze.js
+++ js/maze.js
@@ -1,6 +1,33 @@
 'use strict';
 
-function Maze() {
-	this.width = 7;
-	this.height = 5;
+function Maze(width, height) {
+	this.width = width;
+	this.height = height;
+
+	this.startX = null;
+	this.startY = null;
+	this.startOrientation = null;
+	this.endX = null;
+	this.endY = null;
+
+	this.spaces = [];
+	var x, y;
+	for(x = 1; x <= width; x++) {
+		this.spaces[x] = [];
+	}
+
+	for(y = 1; y <= height; y++) {
+		this.spaces[x][y] = '(' + x + ',' + y + ')';
+	}
 }
+
+Maze.prototype.setStart = function(x, y, orientation) {
+	this.startX = x;
+	this.startY = y;
+	this.startOrientation = orientation;
+};
+
+Maze.prototype.setEnd = function(x, y) {
+	this.endX = x;
+	this.endY = y;
+};
